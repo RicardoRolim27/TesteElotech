8ca1103ece3ddad07cfaf5d9e15c138a1425af1c
ricardo <ricardorolim27@gmail.com>
2021-05-30 15:40:53 -0300

    Implementação da Classe PessoaFisica

---
+++ back-end/src/main/java/elotech/teste/entities/PessoaFisica.java
@@ -0,0 +1,97 @@
+package elotech.teste.entities;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoa_fisica")
+public class PessoaFisica implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private String cpf;
+	
+	private Calendar dataNascimernto;
+	
+	public PessoaFisica() {}
+
+	public PessoaFisica(Long id, String name, String cpf, Calendar dataNascimernto) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.dataNascimernto = dataNascimernto;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Calendar getDataNascimernto() {
+		return dataNascimernto;
+	}
+
+	public void setDataNascimernto(Calendar dataNascimernto) {
+		this.dataNascimernto = dataNascimernto;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PessoaFisica other = (PessoaFisica) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	};
+	
+	
+
+}
