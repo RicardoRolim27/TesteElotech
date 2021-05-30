f739cfe898f3b250d6c789ca7369a2f7e1b05122
ricardo <ricardorolim27@gmail.com>
2021-05-30 18:15:41 -0300

    Implementação da Classe PessoaFisicaRepository

---
+++ back-end/src/main/java/elotech/teste/repositories/PessoaFisicaRepository.java
@@ -0,0 +1,18 @@
+package elotech.teste.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import elotech.teste.entities.Contato;
+import elotech.teste.entities.PessoaFisica;
+
+public interface PessoaFisicaRepository extends JpaRepository<PessoaFisica, Long> {
+	
+	
+	@Query("SELECT DISTINCT obj FROM PessoaFisica JOIN FETCH obj.contatos"
+			+ "WHERE obj.id > 0 ORDER BY obj.name ASC")
+	List<Contato> FindPessoaWithContatos();
+
+}
