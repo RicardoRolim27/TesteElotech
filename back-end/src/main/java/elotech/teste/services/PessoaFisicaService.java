5e3d749c37c8ad002520e1a0c53a56afed987c10
ricardo <ricardorolim27@gmail.com>
2021-05-30 19:09:05 -0300

    Implementação da Classe PessoaFisicaService

---
+++ back-end/src/main/java/elotech/teste/services/PessoaFisicaService.java
@@ -0,0 +1,66 @@
+package elotech.teste.services;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import elotech.teste.dto.ContatoDTO;
+import elotech.teste.dto.PessoaFisicaDTO;
+import elotech.teste.entities.Contato;
+import elotech.teste.entities.PessoaFisica;
+import elotech.teste.repositories.ContatoRepository;
+import elotech.teste.repositories.PessoaFisicaRepository;
+
+@Service
+public class PessoaFisicaService {
+	
+	@Autowired
+	private PessoaFisicaRepository pessoaRepository;
+	@Autowired
+	private ContatoRepository contatoRepository;
+	
+	@Transactional(readOnly = true)
+	public List<PessoaFisicaDTO> findAll(){
+		
+		List<PessoaFisica> list = pessoaRepository.FindPessoaWithContatos();
+		
+		return list.stream().map(x -> new PessoaFisicaDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public PessoaFisicaDTO insert(PessoaFisicaDTO dto) {
+		
+		PessoaFisica pessoa = new PessoaFisica(null, dto.getName(), dto.getCpf(), dto.getDataNascimento());
+		
+		for(ContatoDTO p: dto.getContatos()) {
+			Contato contato = contatoRepository.getById(p.getId());
+			pessoa.getContatos().add(contato);
+		}
+		
+		pessoa = pessoaRepository.save(pessoa);
+		
+		return new PessoaFisicaDTO(pessoa);
+	}
+	
+	@Transactional
+	public PessoaFisicaDTO update(Long id, String name, String cpf, Calendar dataNascimento) {
+		PessoaFisica pessoa = pessoaRepository.getById(id);
+		pessoa.setName(name);
+		pessoa.setCpf(cpf);
+		pessoa.setDataNascimento(dataNascimento);
+		pessoa = pessoaRepository.save(pessoa);
+		
+		return new PessoaFisicaDTO(pessoa);
+	}
+	
+	@Transactional
+	public void delete(Long id) {
+		pessoaRepository.deleteById(id);
+		
+	}
+
+}
