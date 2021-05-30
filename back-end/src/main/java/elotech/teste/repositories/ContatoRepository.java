65c4041ddb9c877cc64a02cc983ea4a82625b6dd
ricardo <ricardorolim27@gmail.com>
2021-05-30 18:19:41 -0300

    Implementação da Classe ContatoRepository

---
+++ back-end/src/main/java/elotech/teste/repositories/ContatoRepository.java
@@ -0,0 +1,13 @@
+package elotech.teste.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import elotech.teste.entities.Contato;
+
+public interface ContatoRepository extends JpaRepository<Contato, Long>{
+	
+	List<Contato> findAllByOrderByNameAsc();
+
+}
