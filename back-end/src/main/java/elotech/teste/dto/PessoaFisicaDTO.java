b478b8cb5fe1796e46c327818e0ef8117579fe9e
ricardo <ricardorolim27@gmail.com>
2021-05-30 17:52:57 -0300

    Implementação da Classe PessoaFisicaDTO

---
+++ back-end/src/main/java/elotech/teste/dto/PessoaFisicaDTO.java
@@ -0,0 +1,71 @@
+package elotech.teste.dto;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+import elotech.teste.entities.PessoaFisica;
+
+public class PessoaFisicaDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String cpf;
+	private Calendar dataNascimento;
+	
+	public PessoaFisicaDTO() {}
+
+	public PessoaFisicaDTO(Long id, String name, String cpf, Calendar dataNascimento) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.dataNascimento = dataNascimento;
+	}
+
+	public PessoaFisicaDTO(PessoaFisica entity) {
+		super();
+		id = entity.getId();
+		name = entity.getName();
+		cpf = entity.getCpf();
+		dataNascimento = entity.getDataNascimento();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Calendar getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Calendar dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	
+	
+	
+
+}
