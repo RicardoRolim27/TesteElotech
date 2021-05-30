e12bc8d13692435efa874a8988724e143294fca3
ricardo <ricardorolim27@gmail.com>
2021-05-30 18:32:14 -0300

    Update PessoaFisicaRepository.java

--- back-end/src/main/java/elotech/teste/repositories/PessoaFisicaRepository.java
+++ back-end/src/main/java/elotech/teste/repositories/PessoaFisicaRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import elotech.teste.entities.Contato;
 import elotech.teste.entities.PessoaFisica;
 
 public interface PessoaFisicaRepository extends JpaRepository<PessoaFisica, Long> {
@@ -13,6 +12,6 @@ public interface PessoaFisicaRepository extends JpaRepository<PessoaFisica, Long
 	
 	@Query("SELECT DISTINCT obj FROM PessoaFisica JOIN FETCH obj.contatos"
 			+ "WHERE obj.id > 0 ORDER BY obj.name ASC")
-	List<Contato> FindPessoaWithContatos();
+	List<PessoaFisica> FindPessoaWithContatos();
 
 }
