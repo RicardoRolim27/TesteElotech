bef126ba78cb0054d0e2713d4ea85b28e84ff3e7
ricardo <ricardorolim27@gmail.com>
2021-05-31 17:51:21 -0300

    Update Routes.tsx

--- front-end/front-web/src/Routes.tsx
+++ front-end/front-web/src/Routes.tsx
@@ -1,10 +1,15 @@
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './Home'
+import CadastroPessoa from './CadastroPessoa';
+import React from 'react';
 function Rountes(){
     return(
         
         <BrowserRouter>
             <Switch>
+            <Route path="/cadastroPessoa">
+                    <CadastroPessoa/>
+                </Route>   
                 <Route path="/">
                     <Home/>
                 </Route>   
