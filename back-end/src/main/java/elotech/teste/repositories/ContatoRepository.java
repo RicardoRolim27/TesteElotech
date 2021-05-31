622f160d27d560f32ed7dcc0889c821880c0cfd3
ricardo <ricardorolim27@gmail.com>
2021-05-31 13:53:34 -0300

    Update ContatoRepository.java

--- back-end/src/main/java/elotech/teste/repositories/ContatoRepository.java
+++ back-end/src/main/java/elotech/teste/repositories/ContatoRepository.java
@@ -1,6 +1,7 @@
 package elotech.teste.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,5 +10,6 @@ import elotech.teste.entities.Contato;
 public interface ContatoRepository extends JpaRepository<Contato, Long>{
 	
 	List<Contato> findAllByOrderByNameAsc();
+	
 
 }
