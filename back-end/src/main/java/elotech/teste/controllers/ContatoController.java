fb429045362d24b708c0917aef32ad1d657acf2f
ricardo <ricardorolim27@gmail.com>
2021-05-31 13:53:23 -0300

    Update ContatoController.java

--- back-end/src/main/java/elotech/teste/controllers/ContatoController.java
+++ back-end/src/main/java/elotech/teste/controllers/ContatoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import elotech.teste.dto.ContatoDTO;
+import elotech.teste.repositories.PessoaFisicaRepository;
 import elotech.teste.services.ContatoService;
 
 @RestController
@@ -23,6 +24,9 @@ public class ContatoController {
 	@Autowired
 	private ContatoService service;
 	
+	@Autowired
+	private PessoaFisicaRepository pessoaRepo;
+	
 	@GetMapping
 	public ResponseEntity<List<ContatoDTO>> findAll(){
 		List<ContatoDTO> list = service.findAll();
@@ -30,11 +34,14 @@ public class ContatoController {
 		return ResponseEntity.ok().body(list);
 	}
 	
-	@PostMapping("/insert/{pessoaId}")
-	public ResponseEntity<ContatoDTO> insert(@RequestBody ContatoDTO dto, @PathVariable Long pessoaID) {
-		dto = service.insert(dto, pessoaID);
+	@PostMapping("/{id}")
+	public ResponseEntity<ContatoDTO> insert(@RequestBody ContatoDTO dto, @PathVariable String id) {
+		
+		Long pessoaId = Long.parseLong(id);
+		
+		dto = service.insert(dto, pessoaId);
 
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId(), pessoaID).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId(), id).toUri();
 		
 		return ResponseEntity.created(uri).body(dto);
 	}
