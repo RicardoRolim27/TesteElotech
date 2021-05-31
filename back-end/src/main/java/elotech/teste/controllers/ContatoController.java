294481ece8c16ce3bbde22b23159b74dbc91c69a
ricardo <ricardorolim27@gmail.com>
2021-05-30 23:22:21 -0300

    Implementação da Classe ContatoDTO

---
+++ back-end/src/main/java/elotech/teste/controllers/ContatoController.java
@@ -0,0 +1,40 @@
+package elotech.teste.controllers;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import elotech.teste.dto.ContatoDTO;
+import elotech.teste.services.ContatoService;
+
+@RestController
+@RequestMapping(value="/contatos")
+public class ContatoController {
+	
+	@Autowired
+	private ContatoService service;
+	
+	@GetMapping
+	public ResponseEntity<List<ContatoDTO>> findAll(){
+		List<ContatoDTO> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ContatoDTO> insert(@RequestBody ContatoDTO dto) {
+		dto = service.insert(dto);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+
+}
