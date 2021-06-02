89f8f473a04e2c19d3fcf1348452b5d46e0e0d34
ricardo <ricardorolim27@gmail.com>
2021-06-02 12:20:22 -0300

    Update ContatoDTO.java

--- back-end/src/main/java/elotech/teste/dto/ContatoDTO.java
+++ back-end/src/main/java/elotech/teste/dto/ContatoDTO.java
@@ -3,7 +3,6 @@ package elotech.teste.dto;
 import java.io.Serializable;
 
 import elotech.teste.entities.Contato;
-import elotech.teste.entities.PessoaFisica;
 
 public class ContatoDTO implements Serializable {
 	
@@ -13,18 +12,16 @@ public class ContatoDTO implements Serializable {
 	private String name;
 	private String phone;
 	private String email;
-	private PessoaFisica pessoaId;
 	
 	public ContatoDTO() {}
 
 	public ContatoDTO(Long id, String name, String phone, 
-			String email, PessoaFisica pessoaId) {
+			String email) {
 		super();
 		this.id 	= id;
 		this.name 	= name;
 		this.phone 	= phone;
 		this.email 	= email;
-		this.pessoaId = pessoaId;
 	}
 
 	public ContatoDTO(Contato entity) {
@@ -33,7 +30,6 @@ public class ContatoDTO implements Serializable {
 		name 	 = entity.getName();
 		phone 	 = entity.getPhone();
 		email    = entity.getEmail();
-		pessoaId = entity.getPessoaId();
 	}
 
 	public Long getId() {
@@ -68,13 +64,6 @@ public class ContatoDTO implements Serializable {
 		this.email = email;
 	}
 
-	public PessoaFisica getPessoaId() {
-		return pessoaId;
-	}
-
-	public void setPessoaId(PessoaFisica pessoaId) {
-		this.pessoaId = pessoaId;
-	}
 	
 	
 }
